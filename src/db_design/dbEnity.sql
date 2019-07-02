ef9e9da529729eebb99479865246f36cad8d38c4
Youggls <462046122@qq.com>
2019-07-02 14:27:33 +0800

    dbenity sql file

---
+++ src/db_design/dbEnity.sql
@@ -0,0 +1,85 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema JavaWeb
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema JavaWeb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `JavaWeb` ;
+USE `JavaWeb` ;
+
+-- -----------------------------------------------------
+-- Table `JavaWeb`.`user`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `JavaWeb`.`user` ;
+
+CREATE TABLE IF NOT EXISTS `JavaWeb`.`user` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nickname` VARCHAR(45) NULL DEFAULT 'null',
+  `password` VARCHAR(50) NOT NULL,
+  `profile_photo_url` VARCHAR(50) NULL DEFAULT 'null',
+  `registered_time` DATE NOT NULL,
+  `type` VARCHAR(10) NOT NULL DEFAULT 'user',
+	CHECK (`type` in ("user", "operator", "admin")),
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `JavaWeb`.`post`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `JavaWeb`.`post` ;
+
+CREATE TABLE IF NOT EXISTS `JavaWeb`.`post` (
+  `post_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `user_id` INT UNSIGNED NOT NULL,
+  `post_name` VARCHAR(100) NOT NULL,
+  `content` TEXT NOT NULL,
+  `post_time` DATE NOT NULL,
+  PRIMARY KEY (`post_id`),
+  INDEX `user_id_idx` (`user_id` ASC) VISIBLE,
+  CONSTRAINT `post_user_id`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `JavaWeb`.`user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `JavaWeb`.`comment`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `JavaWeb`.`comment` ;
+
+CREATE TABLE IF NOT EXISTS `JavaWeb`.`comment` (
+  `comment_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `user_id` INT UNSIGNED NOT NULL,
+  `parent_post_id` INT UNSIGNED NOT NULL,
+  `pre_comment_id` INT NULL DEFAULT -1,
+  `content` TEXT NOT NULL,
+  `comment_time` DATE NOT NULL,
+  PRIMARY KEY (`comment_id`),
+  INDEX `user_id_idx` (`user_id` ASC) VISIBLE,
+  INDEX `post_id_idx` (`parent_post_id` ASC) VISIBLE,
+  CONSTRAINT `commnet_user_id`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `JavaWeb`.`user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `post_id`
+    FOREIGN KEY (`parent_post_id`)
+    REFERENCES `JavaWeb`.`post` (`post_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
