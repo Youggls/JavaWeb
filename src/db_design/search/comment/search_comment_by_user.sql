da0e80fb88039ae0666666d42e620a04de01f7f4
Youggls <462046122@qq.com>
2019-07-03 15:17:37 +0800

    继续给曹续生擦屁股

--- src/db_design/search/comment/search_comment_by_user.sql
+++ src/db_design/search/comment/search_comment_by_user.sql
@@ -1,19 +1,8 @@
 DELIMITER &&
 drop procedure if exists search_comment_by_user;
-create procedure search_comment_by_user(in key_point integer, out mComment_id integer, out mFloor_id integer, out mContent text)
+create procedure search_comment_by_user(in key_point integer)
 deterministic
 reads sql data
 begin
-	#declare Mi integer;
-    #declare Fi integer;
-    #declare Ct text;
-	#select comment_id from comment where user_id = key_point into Mi;
-	#select root_floor_id from comment where user_id = key_point into Fi;
-	#select content from comment where user_id = key_point into Ct;
-
-
-    #set mComment_id = Mi;
-   # set mFloor_id = Fi;
-    #set mContent = Ct;
-    select comment_id, root_floor_id, content from comment where user_id = key_point and isdeleted;
+    select comment_id, user_id, root_floor_id, content from comment where user_id = key_point and isdeleted;
 end &&
---
+++ src/db_design/search/floor/search_floor_by_user.sql
@@ -0,0 +1,8 @@
+DELIMITER &&$
+drop procedure if exists search_floor_by_user;
+create procedure search_floor_by_user(in key_point integer)
+deterministic
+reads sql data
+begin
+    select floor_id, user_id, parent_post_id, floor_num, floor_content from floor  where user_id = key_point;
+end &&$
