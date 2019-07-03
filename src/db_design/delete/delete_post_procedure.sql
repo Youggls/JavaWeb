5f70f9a2c0245ba3d4336669cca6e30e3d2f58c9
Walter White <tjcxs136720@163.com>
2019-07-03 15:48:51 +0800

    Revert "Revert "java bean""
    This reverts commit 05eec510046e545ef3ba4b339d7cc791e353dbef.

--- src/db_design/delete/delete_post_procedure.sql
+++ src/db_design/delete/delete_post_procedure.sql
@@ -7,16 +7,16 @@ begin
 	declare myFloor_id integer;
 	declare numOfFloor integer default 0;
     
-    declare curForComment cursor for select floor_id from floor where parent_post_id = mpost_id;
+    declare curForComment cursor for select floor_id from floor where parentPostId = mpost_id;
     declare continue handler for  SQLSTATE '02000' set myFLoor_id=0;
     
-    select count(*) from floor where parent_post_id = mpost_id into numOfFloor;
+    select count(*) from floor where parentPostId = mpost_id into numOfFloor;
     open curForComment;
     fetch curForComment into myFloor_id;
     while(myFloor_id <> 0) Do 
 		delete from comment where root_floor_id = myFloor_id;
         fetch curForComment into myFloor_id;
 	end while;
-    delete from floor where parent_post_id = mpost_id;
+    delete from floor where parentPostId = mpost_id;
     delete from post where post_id = mpost_id;
 end &&
