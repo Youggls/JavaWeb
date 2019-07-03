4e977240c2cf2544f88435d24e08d3cc20beb03d
Youggls <462046122@qq.com>
2019-07-04 00:21:17 +0800

    fix Dbutil bug

--- src/indi/RDY/JavaWeb/util/DbUtil.java
+++ src/indi/RDY/JavaWeb/util/DbUtil.java
@@ -1,18 +1,25 @@
 package indi.RDY.JavaWeb.util;
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.sql.*;
 import org.apache.commons.io.FileUtils;
 import org.json.JSONObject;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServlet;
+
 public class DbUtil {
     private Connection conn;
     private static String dbUserName;
     private static String dbUserPassWord;
     private static String dbUrl;
 
-    static {
+    public static void init(HttpServlet servlet) {
         String separator = System.getProperty("file.separator");
-        File jsonFile = new File("." + separator + "bin" + separator);
+        ServletContext ctx = servlet.getServletContext();
+        File jsonFile = new File(ctx.getRealPath("WEB-INF" + separator + "classes" + separator + "bin" + separator + "config.json"));
         try {
             String content = FileUtils.readFileToString(jsonFile, "UTF-8");
             JSONObject jsonObject = new JSONObject(content);
