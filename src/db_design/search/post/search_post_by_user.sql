488fce45d6f81046c2bd08479c43ae2cd766bd67
Youggls <462046122@qq.com>
2019-07-05 00:51:19 +0800

    fix search by user_id to search by nickname

--- src/db_design/search/post/search_post_by_user.sql
+++ src/db_design/search/post/search_post_by_user.sql
@@ -1,8 +1,8 @@
 DELIMITER &&#
 drop procedure if exists search_post_by_user;
-create procedure search_post_by_user(in key_point integer)
+create procedure search_post_by_user(in key_point varchar(50))
 #deterministic
 reads sql data
 begin
-    select post_id, user_id, post_name, content, post_time from post where key_point = user_id;
+    select post_id, user_id, post_name, content, post_time from post where key_point = nickname;
 end &&#
--- src/indi/RDY/JavaWeb/servlet/CheckServlet.java
+++ src/indi/RDY/JavaWeb/servlet/CheckServlet.java
@@ -6,6 +6,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 /**
  * Created by user on 17-7-20. * This Servlet suppport register page.
@@ -19,7 +22,7 @@ public class CheckServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("check");
-        String username = request.getParameter("nickname");
+        String username = new String(request.getParameter("nickname").getBytes(StandardCharsets.ISO_8859_1), UTF_8);
         if (username.equals("test")) { //单纯测试，不进行连接数据库，，相同返回true
             response.getWriter().print(true);
         } else {
