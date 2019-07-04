2c94d5b0332b5b7c4f6b2d74b2d08e1f1f40d6e8
Youggls <462046122@qq.com>
2019-07-05 00:55:50 +0800

    fix the encoding problem

--- src/indi/RDY/JavaWeb/util/LogInUtil.java
+++ src/indi/RDY/JavaWeb/util/LogInUtil.java
@@ -1,9 +1,13 @@
 package indi.RDY.JavaWeb.util;
 
 import indi.RDY.JavaWeb.bean.User;
+
 import javax.servlet.http.*;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.sql.*;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class LogInUtil {
     private String nickName;
@@ -24,62 +28,62 @@ public class LogInUtil {
         Timestamp time = null;
         int type = User.VISITOR;
         ResultSet rs = null;
-        if (cookies != null && false) {
-            //The cookies existed
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("nickname")) {
-                    //The saved cookie existed id
-                    nickName = cookie.getValue();
-                    break;
-                }
-            }
-            try {
-                PreparedStatement login = conn.prepareStatement("SELECT * FROM user WHERE nickname = ?");
-                login.setString(1, nickName);
-                login.execute();
-                rs = login.getResultSet();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+//        if (cookies != null && false) {
+//            //The cookies existed
+//            for (Cookie cookie : cookies) {
+//                if (cookie.getName().equals("nickname")) {
+//                    //The saved cookie existed id
+//                    nickName = cookie.getValue();
+//                    break;
+//                }
+//            }
+//            try {
+//                PreparedStatement login = conn.prepareStatement("SELECT * FROM user WHERE nickname = ?");
+//                login.setString(1, nickName);
+//                login.execute();
+//                rs = login.getResultSet();
+//            } catch (SQLException e) {
+//                e.printStackTrace();
+//            }
+//
+//        } else {
+        //Cookie doesn't exist, user should input the id and password
+        try {
+            PreparedStatement login = conn.prepareStatement("SELECT * FROM user WHERE nickname = ? AND password = ?");
+            nickName = new String(req.getParameter("nickname").getBytes(StandardCharsets.ISO_8859_1), UTF_8);
+            password = req.getParameter("password");
+            System.out.println("get from the post is: " + nickName + " " + password);
+            //decodePassword();
+            login.setString(1, nickName);
+            login.setString(2, password);
+            login.execute();
+            rs = login.getResultSet();
 
-        } else {
-            //Cookie doesn't exist, user should input the id and password
-            try {
-                PreparedStatement login = conn.prepareStatement("SELECT * FROM user WHERE nickname = ? AND password = ?");
-                nickName = req.getParameter("nickname");
-                password = req.getParameter("password");
-                System.out.println("get from the post is: " + nickName + " " + password);
-                //decodePassword();
-                login.setString(1, nickName);
-                login.setString(2, password);
-                login.execute();
-                rs = login.getResultSet();
-
-                String info;
-                if (rs.next()) {
-                    //Login succeed!
-                    System.out.println("Get the info from database!");
-                    info = "User: [" + nickName + "] successfully login!";
-                    Cookie cookie = new Cookie("nickname", nickName);
-                    cookie.setPath("/");
-                    //Save two days
-                    cookie.setMaxAge(2 * 24 * 60 * 60);
-                    //Save the cookie
-                    resp.addCookie(cookie);
-                    rs.previous();
-                } else {
-                    info = "User: [" + nickName + "] login failed";
-                }
-                System.out.println(info);
-            } catch (SQLException e) {
-                e.printStackTrace();
+            String info;
+            if (rs.next()) {
+                //Login succeed!
+                System.out.println("Get the info from database!");
+                info = "User: [" + nickName + "] successfully login!";
+                Cookie cookie = new Cookie("nickname", nickName);
+                cookie.setPath("/");
+                //Save two days
+                cookie.setMaxAge(2 * 24 * 60 * 60);
+                //Save the cookie
+                resp.addCookie(cookie);
+                rs.previous();
+            } else {
+                info = "User: [" + nickName + "] login failed";
             }
+            System.out.println(info);
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
+//        }
         try {
             if (rs != null && rs.next()) {
                 //Get user info from the database
                 int id = rs.getInt("id");
-                nickName = rs.getString("nickname");
+                nickName = new String(rs.getString("nickname").getBytes(StandardCharsets.ISO_8859_1), UTF_8);
                 password = rs.getString("password");
                 photoUrl = rs.getString("profile_photo_url");
                 time = rs.getTimestamp("registered_time");
